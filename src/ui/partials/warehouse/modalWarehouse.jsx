44f6fa47954df32dfcf57725b95a69fdfa9a2c69
Werbsson Saymon <64561641+WerbssonSaymon@users.noreply.github.com>
2024-10-10 15:50:05 -0300

    refactor: estilizando o modal

--- src/ui/partials/warehouse/modalWarehouse.jsx
+++ src/ui/partials/warehouse/modalWarehouse.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SelectTypeInterprise from "../../components/selectTypeInterprise";
 import { Button } from "@radix-ui/themes";
 
-export default function modalWarehouse({
+export default function ModalWarehouse({
   currentWarehouse,
   updatedDescription,
   updateType,
@@ -17,43 +17,50 @@ export default function modalWarehouse({
   handleSave,
 }) {
   return (
-    <>
-      <div className="mt-4">
-        <label className="block text-sm font-medium text-gray-700">
-          Descrição:
-        </label>
-        <input
-          type="text"
-          value={currentWarehouse ? updatedDescription : descricao}
-          onChange={(e) =>
-            currentWarehouse
-              ? setUpdatedDescription(e.target.value)
-              : setDescricao(e.target.value)
-          }
-          className="mt-1 px-3 py-2 border shadow-sm block w-full sm:text-sm border-gray-300 rounded-md"
-        />
-      </div>
+    <div className="fixed inset-0 z-50 flex justify-end bg-black bg-opacity-50">
+      <div className="w-full max-w-md h-full bg-white shadow-lg p-6 overflow-y-auto">
+        <h2 className="text-lg font-semibold mb-4">
+          {currentWarehouse ? "Editar Almoxarifado" : "Cadastrar Almoxarifado"}
+        </h2>
 
-      <div className="mt-4">
-        <SelectTypeInterprise
-          value={currentWarehouse ? updateType : tipo}
-          setValue={currentWarehouse ? setUpdateType : setTipo}
-        />
-      </div>
+        <div className="mt-4">
+          <label className="block text-sm font-medium text-gray-700">
+            Descrição:
+          </label>
+          <input
+            type="text"
+            value={currentWarehouse ? updatedDescription : descricao}
+            onChange={(e) =>
+              currentWarehouse
+                ? setUpdatedDescription(e.target.value)
+                : setDescricao(e.target.value)
+            }
+            className="mt-1 px-3 py-2 border shadow-sm block w-full sm:text-sm border-gray-300 rounded-md"
+          />
+        </div>
 
-      <div className="mt-6 flex justify-end">
-        <Button
-          className="mr-2"
-          onClick={() => {
-            clearForm(), setIsDialogOpen(false);
-          }}
-        >
-          Cancelar
-        </Button>
-        <Button onClick={handleSave}>
-          {currentWarehouse ? "Atualizar" : "Cadastrar"}
-        </Button>
+        <div className="mt-4">
+          <SelectTypeInterprise
+            value={currentWarehouse ? updateType : tipo}
+            setValue={currentWarehouse ? setUpdateType : setTipo}
+          />
+        </div>
+
+        <div className="mt-6 flex justify-end">
+          <Button
+            className="mr-2"
+            onClick={() => {
+              clearForm();
+              setIsDialogOpen(false);
+            }}
+          >
+            Cancelar
+          </Button>
+          <Button onClick={handleSave}>
+            {currentWarehouse ? "Atualizar" : "Cadastrar"}
+          </Button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
